96f16be039997da707b50a286c24c4513c21a229
Malcolm Leake <echelon1089@gmail.com>
2020-08-07 19:16:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+
+wakeDog = console.log("Wake ${dogName} the ${dogBreed}");,
+leashDog = console.log("Leash ${dogName} the ${dogBreed}");,
+walkToPark = console.log("Walk to the park with ${dogName} the ${dogBreed}");,
+throwFrisbee = console.log("Throw the frisbee for ${dogName} the ${dogBreed}");,
+walkHome = console.log("Walk home with ${dogName} the ${dogBreed}");,
+unleashDog = console.log("Unleash ${dogName} the ${dogBreed}");
+
+// routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+// const exerciseDog = function(dogName, dogBreed) {
+//   console.log(routine);
+// }
+
